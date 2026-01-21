daf75559ec26333715c2ae85891ee4b4f0bbdd95
Breno Guardia <brenoguardia02@outlook.com>
2026-01-20 23:01:06 -0300

    chore: update metada pages

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,11 @@
 import { ProductList } from "@/components";
+import { Metadata } from "next";
 import { Suspense } from "react";
+
+export const metadata: Metadata = {
+  title: "Omie - Produtos",
+  description: "Tela de listagem dos produtos",
+};
 
 export default function ProductsPage() {
   return (
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,4 +1,10 @@
 import { UserList } from "@/components/user";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Omie - Usuários",
+  description: "Tela de listagem dos usuários",
+};
 
 export default function UsersPage() {
   return <UserList />;
